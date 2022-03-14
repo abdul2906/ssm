30fcf5879dfd408124c94bf909cfaf8fced109f9
abdul2906 <99994548+abdul2906@users.noreply.github.com>
2022-03-14 20:18:20 +0000

    Update cpu stats

--- ssm.c
+++ ssm.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/sysinfo.h>
 #include <X11/Xlib.h>
-
-#define BUFFER_LENGTH 256
 
 int main(void);
 void xsetroot(char* name);
 char* get_time(void);
 float get_mem(void);
-float get_cpu(void);
+int* get_cpu_stat(void);
+float get_cpu(int* last_stat);
 
 int main(void)
 {
     do {
-        char name[256];
+        int* cpu_stat = get_cpu_stat();
+        sleep(1); /* Will break if you wait less than one second */
+        char name[48];
+        char* time = get_time();
         sprintf(name, " [MEM:%.2f%%] [CPU:%.2f%%] [%s] ", 
-            get_mem(), get_cpu(), get_time());
+            get_mem(), get_cpu(cpu_stat), time);
+        free(time);
         xsetroot(name);
-        sleep(1);
     } while (1);
 }
 
@@ -44,19 +46,15 @@ char* get_time(void)
     time_t _time = time(NULL);
     struct tm tm = *localtime(&_time);
 
-    char* out = malloc(sizeof(char)*BUFFER_LENGTH);
+    char* out = malloc(sizeof(char)*32);
     sprintf(out, "%02d:%02d:%02d", tm.tm_hour, tm.tm_min, tm.tm_sec);
     return out;
 }
 
-/* 
-* TODO: Migrate to using sysinfo.h for this
-*/
-
 float get_mem(void)
 {
     FILE *meminfo = fopen("/proc/meminfo", "r");
-    char buffer[BUFFER_LENGTH];
+    char buffer[48];
     float total_ram, free_ram;
     while(fgets(buffer, sizeof(buffer), meminfo)) {
         sscanf(buffer, "MemTotal: %f kB", &total_ram);
@@ -67,15 +65,35 @@ float get_mem(void)
     return (total_ram-free_ram)/total_ram*100;
 }
 
-
-float get_cpu(void)
+int* get_cpu_stat(void)
 {
-    FILE *loadavg = fopen("/proc/loadavg", "r");
-    char buffer[BUFFER_LENGTH];
-    float cpu_avg;
-    while(fgets(buffer, sizeof(buffer), loadavg)) 
-        sscanf(buffer, "%f", &cpu_avg);
+    FILE* stat = fopen("/proc/stat", "r");
+    char buffer[64];
+    int sum = 0;
+    while (fgets(buffer, sizeof(buffer), stat)) {
+        int user, nice, system, idle, iowait, irq, softirq, steal, guest;
+        sscanf(buffer, "cpu %d %d %d %d %d %d %d %d %d", 
+            &user, &nice, &system, &idle, &iowait, &irq, &softirq, &steal, &guest);
+        int _cpu_stat[] = { user, nice, system, idle, iowait, irq, softirq, steal, guest };
 
-    return cpu_avg*100 / get_nprocs_conf();
+        int* cpu_stat = malloc(9*sizeof(int));
+        memcpy(cpu_stat, _cpu_stat, 9*sizeof(int));
+        return cpu_stat;
+    }
 }
 
+float get_cpu(int* last_stat)
+{
+    int* stat = get_cpu_stat();
+    float sum, last_sum = 0.0f;
+    for (int i = 0; i <= 9; i++) {
+        sum += stat[i];
+        last_sum += last_stat[i];
+    }
+    float delta = sum - last_sum;
+    float idle = stat[3]-last_stat[3];
+    float used = delta - idle;
+    free(last_stat);
+
+    return 100*used/delta;
+}
